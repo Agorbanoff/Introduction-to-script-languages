b676f865f95fb752693f0257146c6720cfaddabd
Agorbanoff <alexandargorbanov@gmail.com>
2025-03-26 21:27:34 +0200

    added dependencie to package.json

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>SIGMA SIUU!!!!!</Text>
+      <Text>Sigmaaaaaa</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0000',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
