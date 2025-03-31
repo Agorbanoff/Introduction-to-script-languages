05232f4ca1fc41edea0a2deced87edf15fb42522
Agorbanoff <alexandargorbanov@gmail.com>
2025-03-31 20:10:47 +0300

    fixed margin

--- MobileApp/signUp.js
+++ MobileApp/signUp.js
@@ -123,7 +123,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     alignItems: 'center',
     width: '100%',
-    marginVertical: 5,
+    marginVertical: 10,
   },
   buttonText: {
     color: 'black',
