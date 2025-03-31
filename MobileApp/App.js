389ad6034fdafc703e3a22e314bb5ab3321b5822
Agorbanoff <alexandargorbanov@gmail.com>
2025-03-31 18:06:01 +0300

    changed title and changed background

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LogInPage from './logIn';
@@ -9,12 +9,11 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName="signUp">
-        <Stack.Screen name="logIn" component={LogInPage} options={{title: 'Log in' }} />
-        <Stack.Screen name="signUp" component={SignUpPage} options={{title: 'Sign up' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="signUp">
+                <Stack.Screen name="logIn" component={LogInPage} options={{ title: 'Log in', headerShown: false }} />
+                <Stack.Screen name="signUp" component={SignUpPage} options={{ title: 'Sign up', headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
--- MobileApp/signUp.js
+++ MobileApp/signUp.js
@@ -96,6 +96,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+    marginBottom: '20%'
   },
   input: {
     height: 40,
