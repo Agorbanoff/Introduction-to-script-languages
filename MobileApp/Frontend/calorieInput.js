d2fb327c8fe04c6aa45794b15cede8617b07a444
Agorbanoff <alexandargorbanov@gmail.com>
2025-04-26 23:43:29 +0300

    removed scan bar code button

--- MobileApp/Frontend/calorieInput.js
+++ MobileApp/Frontend/calorieInput.js
@@ -18,19 +18,10 @@ export default function CalorieInput() {
 
   return (
     <View style={styles.container}>
-      {/* Header */}
       <Text style={styles.header}>FOOD LOG</Text>
 
       {/* Action Buttons */}
       <View style={styles.actions}>
-        <TouchableOpacity
-          style={styles.card}
-          onPress={() => navigation.navigate('ScanBarcode')}
-        >
-          <Ionicons name="barcode-outline" size={48} color="#1db344" />
-          <Text style={styles.cardText}>Scan Food</Text>
-        </TouchableOpacity>
-
         <TouchableOpacity
           style={styles.card}
           onPress={() => navigation.navigate('ManualEntry')}
