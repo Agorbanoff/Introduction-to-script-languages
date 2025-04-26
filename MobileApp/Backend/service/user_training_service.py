fad2535502587ea8055dcebb0ec59d8133734208
Agorbanoff <alexandargorbanov@gmail.com>
2025-04-26 14:39:47 +0300

    rewrite user_training service

--- MobileApp/Backend/service/user_training_service.py
+++ MobileApp/Backend/service/user_training_service.py
@@ -2,10 +2,11 @@ from config.db_config import collection_training
 from exceptions.exceptions import StatisticsNotFoundException
 
 async def TimePerWeek(user_id: str, times: int):
-    await collection_training.insert_one({
-        "user_id": user_id,
-        "sessions_per_week": times  
-    })
+    await collection_training.update_one(
+        {"user_id": user_id},
+        {"$set": {"sessions_per_week": times}},
+        upsert=True
+    )
 
 async def getTimePerWeek(user_id: str):
     return await collection_training.find_one({
