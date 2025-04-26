1a9c7a7a92515433ef0a3ed0b1bc0ff00ec584a7
Martin-Boyanov <mboyanov09@gmail.com>
2025-04-27 02:17:15 +0300

    Changed the controller

--- MobileApp/Backend/controller/food_controller.py
+++ MobileApp/Backend/controller/food_controller.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 from service.food_service import fetch_food_info
+from exceptions.exceptions import OpenFoodFactsFetchException
+from fastapi import HTTPException
 
 food_router = APIRouter(tags=["Food"])
 
 @food_router.get("/search")
-async def search_food(query: str):
-    result = await fetch_food_info(query)
-    return result
+def search_food(query: str):
+    try:
+        return fetch_food_info(query)
+    except OpenFoodFactsFetchException:
+        # returns 404 + {"detail":"Item not found"}
+        raise HTTPException(status_code=404, detail="Item not found")
+    except Exception as e:
+        # returns 500 + {"detail": "...error message..."}
+        raise HTTPException(status_code=500, detail=str(e))
